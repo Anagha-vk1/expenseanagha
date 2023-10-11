020abc9f15b97520260f4ca4f3a4ffe7633797cb
Anagha V K <anagha.vk@invenics.com>
2023-10-11 15:26:22 +0530

    alert added when exceeds the limit

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,11 +1,11 @@
 import 'package:expenseanagha/components/expense_summary.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+
 import 'package:shared_preferences/shared_preferences.dart';
 import '../components/expense_tile.dart';
 import '../data/expense_data.dart';
 import '../models/expense_item.dart';
-
 
 class Homepage extends StatefulWidget {
   const Homepage({super.key});
@@ -15,105 +15,120 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
-  final expenseNameController=TextEditingController();
-  final expenserupeeController=TextEditingController();
-  final expensepaisaController=TextEditingController();
+  final expenseNameController = TextEditingController();
+  final expenserupeeController = TextEditingController();
+  final expensepaisaController = TextEditingController();
   TextEditingController limitController=TextEditingController();
-  int? dailyLimit;
+  double dailyLimit = 0.0; // Change the data type to double
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
 
     fetchDailyLimit();
-    Provider.of<ExpenseData>(context,listen: false).prepareData();
-  }
-  Future<void> fetchDailyLimit() async {
-    dailyLimit = await getDailyLimit();
-    setState(() {}); // This triggers a rebuild of the widget.
+    Provider.of<ExpenseData>(context, listen: false).prepareData();
   }
 
-  void addNewExpense(){
-    showDialog(context: context,barrierDismissible: false, builder: (context)=>AlertDialog(title: Text('add new expense'),
-    content: Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        TextField(
-          controller: expenseNameController,
-          decoration: const InputDecoration(
-            hintText: "Expense Name",
-          ),
-        ),
-        Row(
+  Future<void> fetchDailyLimit() async {
+    dailyLimit = await getDailyLimit();
+    setState(() {});
+  }
+
+  void addNewExpense() {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (context) => AlertDialog(
+        title: Text('Add New Expense'),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
           children: [
-            Expanded(
-              child: TextField(
-                keyboardType: TextInputType.number,
-                controller: expenserupeeController,
-                decoration: const InputDecoration(
-                  hintText: "Rupees",
-                ),
+            TextField(
+              controller: expenseNameController,
+              decoration: const InputDecoration(
+                hintText: "Expense Name",
               ),
             ),
-            Expanded(
-              child: TextField(
-                keyboardType: TextInputType.number,
-                controller: expensepaisaController,
-                decoration: const InputDecoration(
-                  hintText: "Paisa",
+            Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    keyboardType: TextInputType.number,
+                    controller: expenserupeeController,
+                    decoration: const InputDecoration(
+                      hintText: "Rupees",
+                    ),
+                  ),
                 ),
-              ),
-            ),
-          ],
-        )
-      ],
-    ),
-    actions: [
-      MaterialButton(onPressed:() {
-        save();
-        Navigator.pop(context);
-        final totalExpenses = calculateTotalExpenses();
-        print(dailyLimit!);
-        print(totalExpenses);// Convert the daily limit input to an integer.
-        if (totalExpenses > dailyLimit!) {
-          // Total expenses exceed the daily limit, show a dialog.
-          showDialog(
-            context: context,
-            builder: (context) => AlertDialog(
-              title: Text('Daily Limit Exceeded'),
-              content: Text(
-                'Total expenses ($totalExpenses) exceed the daily limit ($dailyLimit).',
-              ),
-              actions: [
-                MaterialButton(
-                  onPressed: () {
-                    Navigator.pop(context); // Close the exceeded limit dialog.
-                  },
-                  child: Text('OK'),
+                Expanded(
+                  child: TextField(
+                    keyboardType: TextInputType.number,
+                    controller: expensepaisaController,
+                    decoration: const InputDecoration(
+                      hintText: "Paisa",
+                    ),
+                  ),
                 ),
               ],
-            ),
-          );
-        }
-      },child:const Text('save')),
-      MaterialButton(onPressed: cancel,child:Text('cancel'))
-    ],)
+            )
+          ],
+        ),
+        actions: [
+          MaterialButton(
+            onPressed: () {
+              save();
+              Navigator.pop(context);
+              final totalExpenses = calculateTotalExpenses();
+              print(totalExpenses);
+              if (totalExpenses > dailyLimit) {
+                showDialog(
+                  context: context,
+                  builder: (context) => AlertDialog(
+                    title: Text('Daily Limit Exceeded'),
+                    content: Text(
+                      'Total expenses ($totalExpenses) exceed the daily limit ($dailyLimit).',
+                    ),
+                    actions: [
+                      MaterialButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        child: Text('OK'),
+                      ),
+                    ],
+                  ),
+                );
+              }
+            },
+            child: const Text('Save'),
+          ),
+          MaterialButton(
+            onPressed: cancel,
+            child: Text('Cancel'),
+          ),
+        ],
+      ),
     );
   }
-void deleteExpsense(ExpenseItem expense){
-  Provider.of<ExpenseData>(context,listen: false).deleteExpense(expense);
-}
-  int calculateTotalExpenses() {
-    final List<ExpenseItem> allExpenses = Provider.of<ExpenseData>(context, listen: false).getAllExpenseList();
-    int total = 0;
+
+  void deleteExpense(ExpenseItem expense) {
+    Provider.of<ExpenseData>(context, listen: false).deleteExpense(expense);
+  }
+
+  double calculateTotalExpenses() {
+    final List<ExpenseItem> allExpenses =
+    Provider.of<ExpenseData>(context, listen: false).getAllExpenseList();
+    double total = 0.0; // Initialize total as a double
     for (final expense in allExpenses) {
-      total += int.parse(expense.amount);
+      total += double.parse(expense.amount);
     }
     return total;
   }
-  void save(){
-    print("hiiiiiiii");
-    if(expenserupeeController.text.isNotEmpty && expensepaisaController.text.isNotEmpty && expenseNameController.text.isNotEmpty) {
+
+  void save() {
+    if (expenserupeeController.text.isNotEmpty &&
+        expensepaisaController.text.isNotEmpty &&
+        expenseNameController.text.isNotEmpty) {
       String amount =
           '${expenserupeeController.text}.${expensepaisaController.text}';
       ExpenseItem newExpense = ExpenseItem(
@@ -121,62 +136,78 @@ void deleteExpsense(ExpenseItem expense){
         amount: amount,
         dateTime: DateTime.now(),
       );
-      print("success");
 
-      Provider.of<ExpenseData>(context, listen: false)
-          .addNewExpense(newExpense);
+      Provider.of<ExpenseData>(context, listen: false).addNewExpense(newExpense);
 
       clear();
     }
   }
-  void cancel(){
+
+  void cancel() {
     Navigator.pop(context);
     clear();
   }
-  void clear(){
+
+  void clear() {
     expenseNameController.clear();
     expenserupeeController.clear();
     expensepaisaController.clear();
   }
+
   @override
   Widget build(BuildContext context) {
-    return Consumer<ExpenseData>( builder: (context, value, child) => Scaffold(
-      backgroundColor: Colors.grey[300],
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.black,
-        onPressed: addNewExpense,
-        child: Icon(Icons.add),
-      ),
-    body:ListView(children: [
-      //weeklr summary
-      ExpenseSummary(
-        startOfWeek: value.startOfWeekDate() ?? DateTime.now(),
-      ),
-      Text(
-        'Daily Limit: ${dailyLimit.toString()}',
-        style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-      ),
-      const SizedBox(height: 20),
-      //expense list
-      ListView.builder(
-        shrinkWrap: true,
-        physics: const NeverScrollableScrollPhysics(),
-        itemCount: value.getAllExpenseList().length,
-        itemBuilder: (context, index) => ExpenseTile(
-          name: value.getAllExpenseList()[index].name,
-          amount: value.getAllExpenseList()[index].amount,
-          dateTime: value.getAllExpenseList()[index].dateTime,
-deleteTapped:(p0)=>deleteExpsense(value.getAllExpenseList()[index]) ,
-        ), //ExpenseTile
-      ),
-      ElevatedButton(
-        onPressed: () => _showLimitInputDialog(context),
-        child: Text('Set Daily Limit'),
-      ),
-    ]))
-    );
+    return Consumer<ExpenseData>(
+      builder: (context, value, child) => Scaffold(
+        backgroundColor: Colors.grey[300],
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.black,
+          onPressed: addNewExpense,
+          child: Icon(Icons.add),
+        ),
+        body: Column(
+          children: [
+            Expanded(
+              child: ListView(
+                children: [
+                  // Weekly summary
+                  ExpenseSummary(
+                    startOfWeek: value.startOfWeekDate() ?? DateTime.now(),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top:25.0,left: 16),
+                    child: Text(
+                      'Daily Limit: ${dailyLimit.toStringAsFixed(2)}', // Format the double with two decimal places
+                      style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  // Expense list
+                  ListView.builder(
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    itemCount: value.getAllExpenseList().length,
+                    itemBuilder: (context, index) => ExpenseTile(
+                      name: value.getAllExpenseList()[index].name,
+                      amount: value.getAllExpenseList()[index].amount,
+                      dateTime: value.getAllExpenseList()[index].dateTime,
+                      deleteTapped: (p0) =>
+                          deleteExpense(value.getAllExpenseList()[index]),
+                    ),
+                  ),
 
+                ],
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () => _showLimitInputDialog(context),
+              child: Text('Set Daily Limit'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
+
   Future<void> _showLimitInputDialog(BuildContext context) async {
     await showDialog(
       context: context,
@@ -199,9 +230,8 @@ deleteTapped:(p0)=>deleteExpsense(value.getAllExpenseList()[index]) ,
             TextButton(
               child: Text('Save'),
               onPressed: () {
-                int dailyLimit = int.tryParse(limitController.text) ?? 0;
-                print(dailyLimit);
-                saveDailyLimit(dailyLimit);
+                double newDailyLimit = double.tryParse(limitController.text) ?? 0;
+                saveDailyLimit(newDailyLimit);
                 Navigator.of(context).pop();
               },
             ),
@@ -211,21 +241,16 @@ deleteTapped:(p0)=>deleteExpsense(value.getAllExpenseList()[index]) ,
     );
   }
 
-  void saveDailyLimit(int dailyLimit) async {
+  void saveDailyLimit(double newDailyLimit) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    prefs.setInt('dailyLimit', dailyLimit);
+    prefs.setDouble('dailyLimit', newDailyLimit);
     setState(() {
-      this.dailyLimit = int.tryParse(dailyLimit as String) ?? 0;
+      dailyLimit = newDailyLimit;
     });
-    print(prefs);
   }
 
-  Future<int> getDailyLimit() async {
+  Future<double> getDailyLimit() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    print(prefs.getInt('dailyLimit'));
-    return prefs.getInt('dailyLimit') ?? 0;
-
+    return prefs.getDouble('dailyLimit') ?? 0;
   }
-
 }
-
